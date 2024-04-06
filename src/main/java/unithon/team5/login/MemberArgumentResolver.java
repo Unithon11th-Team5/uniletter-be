3630e49f25297c15287c83307fb6edd5c7708d1c
hong-sile <hyukjoon.dev@gmail.com>
2024-04-06 11:59:36 +0900

    feat: JwtToken에서 Member를 추출해내는 기능 구현

---
+++ src/main/java/unithon/team5/login/MemberArgumentResolver.java
@@ -0,0 +1,40 @@
+package unithon.team5.login;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import unithon.team5.login.service.JwtProvider;
+import unithon.team5.member.Member;
+import unithon.team5.member.repository.MemberRepository;
+
+@Component
+@RequiredArgsConstructor
+public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+  private final JwtProvider jwtProvider;
+  private final MemberRepository memberRepository;
+
+  @Override
+  public boolean supportsParameter(final MethodParameter parameter) {
+    return parameter.getParameterType().equals(Member.class);
+  }
+
+  @Override
+  public Object resolveArgument(
+      final MethodParameter parameter, final ModelAndViewContainer mavContainer,
+      final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory
+  ) {
+    final HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+    final String token = AuthorizationExtractor.extract(request);
+
+    final UUID uuid = jwtProvider.parseMemberId(token);
+    return memberRepository.findById(uuid)
+        .orElseThrow(() -> new IllegalArgumentException("존재하는 멤버를 찾을 수 없습니다."));
+  }
+}
