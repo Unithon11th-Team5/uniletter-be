d57a0a3438ddd7b0954fbf738b8c63d7225d1b30
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-04-06 21:31:54 +0900

    docs: add login info api swagger

--- src/main/java/unithon/team5/login/controller/LoginControllerDocs.java
+++ src/main/java/unithon/team5/login/controller/LoginControllerDocs.java
@@ -1,6 +1,7 @@
 package unithon.team5.login.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -12,6 +13,8 @@ import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.RequestBody;
 import unithon.team5.login.dto.JwtTokenResponse;
 import unithon.team5.login.dto.LoginRequest;
+import unithon.team5.login.dto.LoginResponse;
+import unithon.team5.member.Member;
 
 public interface LoginControllerDocs {
 
@@ -33,4 +36,14 @@ public interface LoginControllerDocs {
                                             }""")
                             })))
     ResponseEntity<JwtTokenResponse> registerToken(@Valid @RequestBody final LoginRequest request);
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "SUCCESS"),
+            @ApiResponse(responseCode = "403", description = "FORBIDDEN",
+                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
+    })
+    @Operation(summary = "로그인한 사용자 정보 조회")
+    ResponseEntity<LoginResponse> getLoginInfo(
+            @Parameter(hidden = true)
+            final Member member);
 }
