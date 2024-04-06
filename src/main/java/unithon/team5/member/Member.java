5e0b7d47aaac71d3cee3422bb03cc74c7009eea6
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-04-06 10:43:46 +0900

    feat: add member entity

---
+++ src/main/java/unithon/team5/member/Member.java
@@ -0,0 +1,32 @@
+package unithon.team5.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import unithon.team5.common.BaseEntity;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+public class Member extends BaseEntity {
+
+    protected Member() {
+        super(null);
+    }
+
+    private Member(final UUID id, final String nickname, final String credential) {
+        super(id);
+        this.nickname = nickname;
+        this.credential = credential;
+    }
+
+    @Column(unique = true, nullable = false)
+    private String nickname;
+
+    private String credential;
+
+    public static Member create(final String nickname, final String credential) {
+        return new Member(null, nickname, credential);
+    }
+}
---
+++ src/main/java/unithon/team5/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package unithon.team5.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import unithon.team5.member.Member;
+
+import java.util.UUID;
+
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+
+}
