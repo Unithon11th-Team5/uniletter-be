94fefb210815ee377c9316c9de9502292600f790
hong-sile <hyukjoon.dev@gmail.com>
2024-04-07 03:36:24 +0900

    feat: Member 프로필 조회 docs 추가

--- src/main/java/unithon/uniletter/member/controller/MemberController.java
+++ src/main/java/unithon/uniletter/member/controller/MemberController.java
@@ -5,12 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import unithon.uniletter.member.Member;
 import unithon.uniletter.member.dto.MemberResponse;
 import unithon.uniletter.member.service.MemberService;
 
 @RequiredArgsConstructor
 @RestController
-public class MemberController {
+public class MemberController implements MemberControllerDocs {
 
     private final MemberService memberService;
 
