80e294a1e9f4e1deb3bca2cdb9caf4e916013793
hong-sile <hyukjoon.dev@gmail.com>
2024-04-06 16:35:38 +0900

    feat: idToken을 통해서 유저 식별하느 기능 추가

--- src/main/java/unithon/team5/login/service/LoginService.java
+++ src/main/java/unithon/team5/login/service/LoginService.java
@@ -1,36 +1,40 @@
 package unithon.team5.login.service;
 
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import unithon.team5.login.OauthMemberInfo;
-import unithon.team5.login.dto.LoginRequest;
 import unithon.team5.member.Member;
 import unithon.team5.member.repository.MemberRepository;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
 
-  private final JwtProvider jwtProvider;
-  private final MemberRepository memberRepository;
-  private final AppleOauthMemberClient appleOauthMemberClient;
+    private final JwtProvider jwtProvider;
+    private final MemberRepository memberRepository;
 
-  @Transactional
-  public String createToken(final LoginRequest loginRequest) {
-    final OauthMemberInfo memberInfo = appleOauthMemberClient.getIdentifier(loginRequest);
-    final Member registeredMember = memberRepository.findByCredential(memberInfo.credential())
-        .orElseGet(() -> registerMember(memberInfo));
-    final UUID id = registeredMember.getId();
-    return jwtProvider.createAccessTokenWith(id);
-  }
+    @Transactional
+    public String createToken(final String idToken, final String email) {
+        final String memberAppleIdentifier = null;
+        final Member registeredMember = memberRepository.findByCredential(memberAppleIdentifier)
+                .orElseGet(() -> registerMember(memberAppleIdentifier, email));
+        final UUID id = registeredMember.getId();
+        return jwtProvider.createAccessTokenWith(id);
+    }
 
-  private Member registerMember(final OauthMemberInfo memberInfo) {
-    final Member member = Member.builder()
-        .nickname(memberInfo.nickName())
-        .credential(memberInfo.credential())
-        .build();
-    return memberRepository.save(member);
-  }
+    private Member registerMember(final String memberAppleIdentifier, final String email) {
+        final String nickname = parseEmail(email);
+        final Member member = Member.builder()
+                .nickname(nickname)
+                .credential(memberAppleIdentifier)
+                .build();
+        return memberRepository.save(member);
+    }
+
+    private String parseEmail(final String email) {
+        int atIndex = email.indexOf('@');
+        return email.substring(0, atIndex);
+    }
 }
