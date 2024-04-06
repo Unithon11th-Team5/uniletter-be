004395e76f83530170263c33f224aa917a3b439c
hong-sile <hyukjoon.dev@gmail.com>
2024-04-06 12:12:17 +0900

    feat: 멤버가 없으면 생성하고 있는 경우 조회하는 기능 구현

--- src/main/java/unithon/team5/member/repository/MemberRepository.java
+++ src/main/java/unithon/team5/member/repository/MemberRepository.java
@@ -1,10 +1,11 @@
 package unithon.team5.member.repository;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import unithon.team5.member.Member;
 
-import java.util.UUID;
-
 public interface MemberRepository extends JpaRepository<Member, UUID> {
 
+  Optional<Member> findByCredential(final String credential);
 }
