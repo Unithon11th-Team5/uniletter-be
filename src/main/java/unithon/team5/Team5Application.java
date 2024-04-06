0cd4c67c53e8e69f49d3191524c08716dd79492e
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-04-06 16:41:47 +0900

    hotfix: test to trigger deploy.yml

--- src/main/java/unithon/team5/Team5Application.java
+++ src/main/java/unithon/team5/Team5Application.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Team5Application {
 
   public static void main(String[] args) {
+    System.out.println("test");
     SpringApplication.run(Team5Application.class, args);
   }
 }
