b790920b6c51798c71e78d785361903bc0842b4e
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-04-07 04:23:21 +0900

    setting: add bearer header to swagger config

--- src/main/java/unithon/uniletter/config/SwaggerConfig.java
+++ src/main/java/unithon/uniletter/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package unithon.uniletter.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +13,22 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+
         return new OpenAPI()
-                .components(new Components())
                 .addServersItem(new Server().url("/"))
-                .info(getPayoutServerInfo());
+                .info(getUniletterServerInfo())
+                .components(
+                        new Components().addSecuritySchemes(
+                                "oauth2 token",
+                                new SecurityScheme()
+                                        .scheme("bearer")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .bearerFormat("jwt")
+                                        .name("oauth2")
+                        ));
     }
 
-    private Info getPayoutServerInfo() {
+    private Info getUniletterServerInfo() {
         return new Info().title("Uniletter Server API")
                 .description("Uniletter Server API 명세서입니다.")
                 .version("1.0.0");
