1b5515a663015fdaeffd2fe87f672e0013d5fbfa
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-04-06 13:05:05 +0900

    feat: add send message service

---
+++ src/main/java/unithon/team5/message/dto/MessageRequest.java
@@ -0,0 +1,29 @@
+package unithon.team5.message.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import unithon.team5.message.MessageType;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageRequest {
+
+    @NotEmpty
+    private String receiverNickname;
+    @NotEmpty
+    private String senderName;
+    @NotEmpty
+    private String content;
+    @NotNull
+    private MessageType type;
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime sendPlannedAt;
+}
