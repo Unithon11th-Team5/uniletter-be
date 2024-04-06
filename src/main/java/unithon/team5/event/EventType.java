a495557bd8275e4dc82db4e55ca33e4724b39eaa
hong-sile <hyukjoon.dev@gmail.com>
2024-04-06 19:34:37 +0900

    feat: EventType 필드값 추가

--- src/main/java/unithon/team5/event/EventType.java
+++ src/main/java/unithon/team5/event/EventType.java
@@ -1,9 +1,23 @@
 package unithon.team5.event;
 
+import lombok.Getter;
+
+@Getter
 public enum EventType {
-    CHEER,
-    BIRTHDAY,
-    HOLIDAY,
-    CHRISTMAS,
-    NONE
+
+    CHEER("응원", "CHEER UP"),
+    BIRTHDAY("생일", "BIRTHDAY"),
+    THANKS_GIVING("명절", "THANKS_GIVING"),
+    CHRISTMAS("연말", "CHRISTMAS"),
+    MARRIAGE("결혼", "MARRIAGE"),
+    NON_EVENT("일상", "NON_EVENT");
+
+    private final String koreanName;
+    private final String englishName;
+
+    EventType(String koreanName, String englishName) {
+        this.koreanName = koreanName;
+        this.englishName = englishName;
+    }
+
 }
