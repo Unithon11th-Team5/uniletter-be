b7ba2f64964700131d8c9001dd95f69ec67bbcaf
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-04-06 11:42:45 +0900

    feat: add message type and isRead column

--- src/main/java/unithon/team5/message/Message.java
+++ src/main/java/unithon/team5/message/Message.java
@@ -2,6 +2,8 @@ package unithon.team5.message;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 import unithon.team5.common.BaseEntity;
 
@@ -20,13 +22,16 @@ public class Message extends BaseEntity {
                     final UUID receiverId,
                     final String senderName,
                     final String content,
+                    final MessageType type,
                     final LocalDateTime sendPlannedAt) {
         super(null);
         this.senderId = senderId;
         this.receiverId = receiverId;
         this.senderName = senderName;
         this.content = content;
+        this.type = type;
         this.sendPlannedAt = sendPlannedAt;
+        this.isRead = false;
     }
 
     @Column(nullable = false, updatable = false)
@@ -40,14 +45,20 @@ public class Message extends BaseEntity {
 
     private String content;
 
+    @Enumerated(EnumType.STRING)
+    private MessageType type;
+
     @Column(nullable = false)
     private LocalDateTime sendPlannedAt;
+    
+    private Boolean isRead;
 
     public static Message create(final UUID senderId,
                                  final UUID receiverId,
                                  final String senderName,
                                  final String content,
+                                 final MessageType type,
                                  final LocalDateTime sendPlannedAt) {
-        return new Message(senderId, receiverId, senderName, content, sendPlannedAt);
+        return new Message(senderId, receiverId, senderName, content, type, sendPlannedAt);
     }
 }
