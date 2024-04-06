16fac16b4f45d7aea025820584e0c9415cc7905b
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-04-06 22:26:04 +0900

    fix: fix url of login info api

--- src/main/java/unithon/team5/login/controller/LoginController.java
+++ src/main/java/unithon/team5/login/controller/LoginController.java
@@ -3,10 +3,7 @@ package unithon.team5.login.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import unithon.team5.login.dto.JwtTokenResponse;
 import unithon.team5.login.dto.LoginRequest;
 import unithon.team5.login.dto.LoginResponse;
@@ -15,11 +12,12 @@ import unithon.team5.member.Member;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/login")
 public class LoginController implements LoginControllerDocs {
 
     private final LoginService loginService;
 
-    @PostMapping("/login/apple")
+    @PostMapping("/apple")
     public ResponseEntity<JwtTokenResponse> registerToken(
             @Valid @RequestBody final LoginRequest loginRequest
     ) {
