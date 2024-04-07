5d1f02c47d5a758656dfb0af54bc534daab1a527
hong-sile <hyukjoon.dev@gmail.com>
2024-04-07 09:56:20 +0900

    fix: RequestBody 추가

--- src/main/java/unithon/uniletter/time/controller/TimeController.java
+++ src/main/java/unithon/uniletter/time/controller/TimeController.java
@@ -3,6 +3,7 @@ package unithon.uniletter.time.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import unithon.uniletter.time.TimeRequest;
 import unithon.uniletter.time.service.TimeService;
@@ -14,7 +15,7 @@ public class TimeController {
     private final TimeService timeService;
 
     @PostMapping("/time")
-    public ResponseEntity<Void> setServerTime(final TimeRequest timeRequest) {
+    public ResponseEntity<Void> setServerTime(@RequestBody final TimeRequest timeRequest) {
         timeService.setServerTime(timeRequest.localDate());
         return ResponseEntity.ok().build();
     }
